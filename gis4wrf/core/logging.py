4f7e399fbe4ea7832f38b5ea56328af0f875bfe5
Maik Riechert <maik.riechert@arcor.de>
2018-08-10 23:14:34 +0100

    add logging capability

---
+++ gis4wrf/core/logging.py
@@ -0,0 +1,8 @@
+# GIS4WRF (https://doi.org/10.5281/zenodo.1288569)
+# Copyright (c) 2018 D. Meyer and M. Riechert. Licensed under MIT.
+
+import logging
+
+logger = logging.getLogger('gis4wrf') # type: logging.Logger
+logger.setLevel(logging.DEBUG)
+logger.propagate = False
