7442c4d751e3fb4dc1ece276651eb00d3b00b2c3
Maik Riechert <maik.riechert@arcor.de>
2018-09-18 21:31:20 +0100

    check /usr/local/bin for mpiexec + refactoring (fixes #44)

--- gis4wrf/core/program.py
+++ gis4wrf/core/program.py
@@ -8,21 +8,52 @@ import time
 
 from gis4wrf.core.util import export
 
+STARTUPINFO = None
+if os.name == 'nt':
+    # hides the console window
+    STARTUPINFO = subprocess.STARTUPINFO()
+    STARTUPINFO.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
+@export
+def find_mpiexec() -> str:
+    plat = platform.system()
+
+    paths = []
+
+    if plat == 'Windows':
+        help_option = []
+        if 'MSMPI_BIN' in os.environ:
+            paths.append(os.path.join(os.environ['MSMPI_BIN'], 'mpiexec.exe'))
+    elif plat in ['Darwin', 'Linux']:
+        help_option = ['-h']
+        paths.append('mpiexec')
+        # Sometimes /usr/local/bin is not in PATH.
+        paths.append('/usr/local/bin/mpiexec')
+    else:
+        raise RuntimeError('Unsupported platform')
+    
+    mpiexec_path = None
+    for path in paths:
+        try: 
+            subprocess.check_output([path] + help_option, startupinfo=STARTUPINFO)
+        except FileNotFoundError:
+            pass
+        else:
+            mpiexec_path = path
+            break
+    
+    if mpiexec_path is None:
+        raise RuntimeError('MPI not found')
+    
+    return mpiexec_path
+
 @export
 def run_program(path: str, cwd: str, error_pattern: Optional[str]=None,
                 use_mpi: bool=False, mpi_processes: Optional[int]=None) -> Iterable[Tuple[str,Any]]:
     if use_mpi:
         if mpi_processes is None:
             mpi_processes = multiprocessing.cpu_count()
-        plat = platform.system()
-        if plat == 'Windows':
-            # Microsoft MPI
-            mpi_path = os.path.join(os.environ['MSMPI_BIN'], 'mpiexec.exe')
-        elif plat in ['Darwin', 'Linux']:
-            # MPICH (most likely)
-            mpi_path = 'mpiexec'
-        else:
-            raise NotImplementedError('MPI not supported on ' + plat)
+        mpi_path = find_mpiexec()
         args = [mpi_path, '-n', str(mpi_processes), path]
     else:
         args = [path]
@@ -32,16 +63,12 @@ def run_program(path: str, cwd: str, error_pattern: Optional[str]=None,
 def _run_program(args: List[str], cwd: str, error_pattern: Optional[str]=None) -> Iterable[Tuple[str,Any]]:
     yield ('log', 'Command: ' + ' '.join(args))
     yield ('log', 'Working directory: ' + cwd)
-    startupinfo = None
-    if os.name == 'nt':
-        # hides the console window
-        startupinfo = subprocess.STARTUPINFO()
-        startupinfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+
     t0 = time.time()
     process = subprocess.Popen(args, cwd=cwd,
                              stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
                              bufsize=1, universal_newlines=True,
-                             startupinfo=startupinfo)
+                             startupinfo=STARTUPINFO)
     yield ('pid', process.pid)
     stdout = ''
     while True:
