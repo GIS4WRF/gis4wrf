35595d8f5bcd21a4f9bb0331d15059e7f3fca186
Maik Riechert <maik.riechert@arcor.de>
2018-09-27 23:04:06 +0100

    fix stale UI when reopening same netcdf file (fixes #79) Also, changed properties to regular functions as Qt was hiding exceptions happening in the property functions, likely to do with the native round-trip to C++.

--- gis4wrf/plugin/ui/widget_view.py
+++ gis4wrf/plugin/ui/widget_view.py
@@ -119,14 +119,18 @@ class ViewWidget(QWidget):
         self.datasets[dataset_name] = Dataset(dataset_name, path, variables, times, extra_dims)
         if is_new_dataset:
             self.dataset_selector.addItem(dataset_name, dataset_name)
-        self.select_dataset(dataset_name)
+        self.select_dataset(dataset_name, is_new=is_new_dataset)
 
-    def select_dataset(self, dataset_name: str) -> None:
+    def select_dataset(self, dataset_name: str, is_new: bool) -> None:
         index = self.dataset_selector.findData(dataset_name)
-        self.dataset_selector.setCurrentIndex(index)
+        current_index = self.dataset_selector.currentIndex()
+        if index == current_index and not is_new:
+            # otherwise the event handler wouldn't be triggered
+            self.dataset_selector.setCurrentIndex(-1)
+        self.dataset_selector.setCurrentIndex(index)     
 
     def init_variable_selector(self) -> None:
-        dataset = self.dataset
+        dataset = self.get_dataset()
         selected = self.selected_variable.get(dataset.name)
         self.variable_selector.clear()
         for var_name, variable in sorted(dataset.variables.items(), key=lambda v: v[1].label):
@@ -134,12 +138,12 @@ class ViewWidget(QWidget):
             item.setData(Qt.UserRole, var_name)
             self.variable_selector.addItem(item)
             if var_name == selected:
-                item.setSelected(True)
+                self.variable_selector.setCurrentItem(item)
         if selected is None:
             self.extra_dim_container.hide()
 
     def init_time_selector(self) -> None:
-        dataset = self.dataset
+        dataset = self.get_dataset()
         self.time_selector.setMaximum(len(dataset.times) - 1)
         selected_time = self.selected_time.get(dataset.name, 0)
         self.select_time(selected_time)
@@ -150,8 +154,8 @@ class ViewWidget(QWidget):
         self.time_selector.setValue(index)
 
     def init_extra_dim_selector(self) -> None:
-        dataset = self.dataset
-        variable = self.variable
+        dataset = self.get_dataset()
+        variable = self.get_variable()
         extra_dim_name = variable.extra_dim_name
         if extra_dim_name is None:
             self.extra_dim_container.hide()
@@ -172,7 +176,7 @@ class ViewWidget(QWidget):
         if dataset_init:
             self.interp_vert_selector.clear()
             has_vert = False
-            sorted_variables = sorted(self.dataset.variables.values(), key=lambda v: v.label)
+            sorted_variables = sorted(self.get_dataset().variables.values(), key=lambda v: v.label)
             for variable in sorted_variables:
                 if variable.extra_dim_name != 'bottom_top':
                     continue
@@ -186,7 +190,7 @@ class ViewWidget(QWidget):
                 self.extra_dim_container.setEnabled(True)
                 self.interp_container.hide()
         else:
-            variable = self.variable
+            variable = self.get_variable()
             extra_dim_name = variable.extra_dim_name
             if extra_dim_name != 'bottom_top':
                 self.interp_container.hide()
@@ -194,21 +198,31 @@ class ViewWidget(QWidget):
             self.interp_container.show()
         
     def on_dataset_selected(self, index: int) -> None:
+        if index == -1:
+            return
+        
         self.init_variable_selector()
         self.init_time_selector()
         self.init_interp_input(True)
         
         previous_dataset = self.selected_dataset
+        self.selected_dataset = self.get_dataset_name()
+
         if previous_dataset is not None:
             gis4wrf.plugin.geo.remove_group(previous_dataset)
-        self.selected_dataset = self.dataset_name
+
+        if previous_dataset == self.selected_dataset:
+            # User re-opened same file, e.g. to see new time steps while running simulation.
+            # Try to load the same variable and time step.
+            self.replace_variable_layer()
+            self.select_time_band_in_variable_layers() 
 
     def on_variable_selected(self, current: Optional[QListWidgetItem], previous: Optional[QListWidgetItem]) -> None:
         if current is None:
             return
-        dataset = self.dataset
         var_name = current.data(Qt.UserRole)
-        assert var_name == self.var_name
+        dataset = self.get_dataset()
+        assert var_name == self.get_var_name()
         self.selected_variable[dataset.name] = var_name
         self.init_extra_dim_selector()
         self.init_interp_input(False)
@@ -216,7 +230,7 @@ class ViewWidget(QWidget):
         self.select_time_band_in_variable_layers()
         
     def on_time_selected(self, index: int) -> None:
-        dataset = self.dataset
+        dataset = self.get_dataset()
         self.selected_time[dataset.name] = index
         self.time_label.setText('Time: ' + dataset.times[index])
         self.select_time_band_in_variable_layers()
@@ -225,9 +239,9 @@ class ViewWidget(QWidget):
         if index == -1:
             # happens when clearing the dropdown entries
             return
-        variable = self.variable
+        variable = self.get_variable()
         extra_dim_name = variable.extra_dim_name
-        self.selected_extra_dim[(self.dataset_name, extra_dim_name)] = index
+        self.selected_extra_dim[(self.get_dataset_name(), extra_dim_name)] = index
         self.replace_variable_layer()
         self.select_time_band_in_variable_layers()
 
@@ -242,13 +256,13 @@ class ViewWidget(QWidget):
     def replace_variable_layer(self) -> None:
         if self.pause_replace_layer:
             return
-        if self.interp_enabled and self.interp_level is None:
+        if self.is_interp_enabled() and self.get_interp_level() is None:
             return
-        dataset = self.dataset
-        variable = self.variable
-        extra_dim_index = self.extra_dim_index
-        interp_level = self.interp_level
-        interp_vert_name = self.interp_vert_name
+        dataset = self.get_dataset()
+        variable = self.get_variable()
+        extra_dim_index = self.get_extra_dim_index()
+        interp_level = self.get_interp_level()
+        interp_vert_name = self.get_interp_vert_name()
         if interp_level is not None:
             extra_dim_index = None
         uri, dispose = gis4wrf.core.convert_wrf_nc_var_to_gdal_dataset(
@@ -258,56 +272,47 @@ class ViewWidget(QWidget):
         dispose_after_delete(layer, dispose)
 
     def select_time_band_in_variable_layers(self) -> None:
-        dataset = self.dataset
-        time_idx = self.time_index
+        dataset = self.get_dataset()
+        time_idx = self.get_time_index()
         layers = gis4wrf.plugin.geo.get_raster_layers_in_group(dataset.name)
         for layer in layers:
             var_name = layer.shortName()
             if var_name in dataset.variables:
                 gis4wrf.plugin.geo.switch_band(layer, time_idx)
     
-    @property
-    def dataset_name(self) -> str:
+    def get_dataset_name(self) -> str:
         return self.dataset_selector.currentData()
 
-    @property
-    def var_name(self) -> str:
+    def get_var_name(self) -> str:
         return self.variable_selector.currentItem().data(Qt.UserRole)   
 
-    @property
-    def time_index(self) -> int:
+    def get_time_index(self) -> int:
         return self.time_selector.value()
 
-    @property
-    def extra_dim_index(self) -> Optional[int]:
-        if self.variable.extra_dim_name is None:
+    def get_extra_dim_index(self) -> Optional[int]:
+        if self.get_variable().extra_dim_name is None:
             return None
         index = self.extra_dim_selector.currentIndex()
         assert index != -1
         return index
 
-    @property
-    def interp_enabled(self):
+    def is_interp_enabled(self):
         return self.interp_container.isVisible() and self.interp_container.isChecked()
 
-    @property
-    def interp_vert_name(self):
-        if not self.interp_enabled:
+    def get_interp_vert_name(self):
+        if not self.is_interp_enabled():
             return None
         return self.interp_vert_selector.currentData()
 
-    @property
-    def interp_level(self) -> Optional[float]:
-        if not self.interp_enabled:
+    def get_interp_level(self) -> Optional[float]:
+        if not self.is_interp_enabled():
             return None
         if not self.interp_input.is_valid():
             return None
         return self.interp_input.value()
 
-    @property
-    def dataset(self) -> Dataset:
-        return self.datasets[self.dataset_name]
+    def get_dataset(self) -> Dataset:
+        return self.datasets[self.get_dataset_name()]
 
-    @property
-    def variable(self) -> WRFNetCDFVariable:
-        return self.dataset.variables[self.var_name]
+    def get_variable(self) -> WRFNetCDFVariable:
+        return self.get_dataset().variables[self.get_var_name()]
